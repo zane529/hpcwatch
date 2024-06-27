e4c5bf50bc587a880c4771e39306226aac0cd169
aws-hcls <11653979+aws-hcls@user.noreply.gitee.com>
2024-06-27 17:51:26 +0800

    add file

---
+++ core/collectNodePrometheus.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+from nodeinfo import NodeResourceV2
+from pushToGateway import push_info
+import awsUtil
+from prometheus_client import CollectorRegistry, Gauge
+
+# Init node info.
+node = NodeResourceV2()
+
+class CollectNode(object):
+    """
+    Collect node info.
+    """
+
+    def collect_node_cpu_info(self):
+        """
+        Collect host cpu.
+        :return:
+        """
+        
+        instance_id = awsUtil.get_aws_instance_id()
+        instance_type = awsUtil.get_aws_instance_type()
+        cpu_info = node.get_cpu_state()
+        
+        registry = CollectorRegistry()
+                
+        g_node_cpu_use_percent = Gauge('node_cpu_use_percent', 'The cpu use of node', ['host', 'instance_type'], registry=registry)
+        g_node_cpu_use_percent.labels(host=instance_id, instance_type=instance_type).set(cpu_info.get('cpu_percent'))
+        
+        g_node_cpu_use_load5 = Gauge('node_cpu_loadavg_5', 'The cpu use of node', ['host', 'instance_type'], registry=registry)
+        g_node_cpu_use_load5.labels(host=instance_id, instance_type=instance_type).set(cpu_info.get('loadavg_5'))
+        
+        g_node_cpu_use_load10 = Gauge('node_cpu_loadavg_10', 'The cpu use of node', ['host', 'instance_type'], registry=registry)
+        g_node_cpu_use_load10.labels(host=instance_id, instance_type=instance_type).set(cpu_info.get('loadavg_10'))
+        
+        g_node_cpu_use_load15 = Gauge('node_cpu_loadavg_15', 'The cpu use of node', ['host', 'instance_type'], registry=registry)
+        g_node_cpu_use_load15.labels(host=instance_id, instance_type=instance_type).set(cpu_info.get('loadavg_15'))
+        
+        push_info('node_cpu_use', registry)
+        
+
+    def collect_node_mem_info(self):
+        """
+        Collect node memory.
+        """
+        instance_id = awsUtil.get_aws_instance_id()
+        instance_type = awsUtil.get_aws_instance_type()
+        mem_info = node.get_memory_state()
+            
+        registry = CollectorRegistry()
+                
+        mem_use = Gauge('mem_used', 'The cpu use of node', ['host', 'instance_type'], registry=registry)
+        mem_use.labels(host=instance_id, instance_type=instance_type).set(mem_info.get('mem_used'))
+        
+        mem_per_used = Gauge('mem_used_per', 'The cpu use of node', ['host', 'instance_type'], registry=registry)
+        mem_per_used.labels(host=instance_id, instance_type=instance_type).set(mem_info.get('mem_used_per'))
+        
+        push_info('node_mem_use', registry)
+        
+
+    def collect_node_disk_info(self):
+        """
+        Collect node disk.
+        """
+        instance_id = awsUtil.get_aws_instance_id()
+        instance_type = awsUtil.get_aws_instance_type()
+        disk_info = node.get_disk_state()
+                
+        registry = CollectorRegistry()
+                
+        disk_used = Gauge('disk_used', 'The cpu use of node', ['host', 'instance_type'], registry=registry)
+        disk_used.labels(host=instance_id, instance_type=instance_type).set(disk_info.get('disk_used'))
+        
+        disk_used_per = Gauge('mem_used_per', 'The cpu use of node', ['host', 'instance_type'], registry=registry)
+        disk_used_per.labels(host=instance_id, instance_type=instance_type).set(disk_info.get('disk_used_per'))
+        
+        push_info('node_disk_use', registry)
+
+
+if __name__ == '__main__':
+    c = CollectNode()
+    c.collect_node_cpu_info()
+    c.collect_node_mem_info()
+    c.collect_node_disk_info()
---
+++ core/pushToGateway.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from prometheus_client import push_to_gateway
+from prometheus_client.exposition import basic_auth_handler
+import awsUtil
+
+HOST = awsUtil.get_aws_parameter('InfluxdbPrivateIp')
+PORT = 9091
+USER = awsUtil.get_aws_parameter('InfluxdbUser')
+PASS = awsUtil.get_aws_parameter('InfluxdbPass')
+DBNAME = 'hpccollect'
+
+
+def auth_handler(url, method, timeout, headers, data):
+    return basic_auth_handler(url, method, timeout, headers, data, USER, PASS)
+
+
+def push_info(job, registry):
+    """
+    Get influxdb client.
+    :return:
+    """
+    
+    try:
+        push_to_gateway('%s:%s' %(HOST, PORT), job=job, registry=registry, handler=auth_handler)
+    except Exception as e:
+        print('Push message error !!!')
+        print(e)
+

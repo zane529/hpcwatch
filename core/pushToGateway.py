213dde2a20e630d0c97fc300316594c9f9445062
aws-hcls <11653979+aws-hcls@user.noreply.gitee.com>
2024-06-27 18:21:24 +0800

    add instance meta

--- core/pushToGateway.py
+++ core/pushToGateway.py
@@ -3,12 +3,15 @@ from prometheus_client import push_to_gateway
 from prometheus_client.exposition import basic_auth_handler
 import awsUtil
 
-HOST = awsUtil.get_aws_parameter('InfluxdbPrivateIp')
-PORT = 9091
-USER = awsUtil.get_aws_parameter('InfluxdbUser')
-PASS = awsUtil.get_aws_parameter('InfluxdbPass')
-DBNAME = 'hpccollect'
+# HOST = awsUtil.get_aws_parameter('InfluxdbPrivateIp')
+# PORT = 9091
+# USER = awsUtil.get_aws_parameter('InfluxdbUser')
+# PASS = awsUtil.get_aws_parameter('InfluxdbPass')
 
+HOST = '10.0.0.132'
+PORT = 9091
+USER = 'admin'
+PASS = 'du@15a7pxdEGVyC'
 
 def auth_handler(url, method, timeout, headers, data):
     return basic_auth_handler(url, method, timeout, headers, data, USER, PASS)

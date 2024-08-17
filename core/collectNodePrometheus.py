5fb57a1ea0fd20709cbe13bea574b87560f3aa4d
Chen <189380064829@163.com>
2024-08-17 18:06:54 +0800

    add debug message

--- core/collectNodePrometheus.py
+++ core/collectNodePrometheus.py
@@ -27,17 +27,17 @@ class CollectNode(object):
         
         registry = CollectorRegistry()
                 
-        g_node_cpu_use_percent = Gauge('node_cpu_use_percent', 'The cpu use of node', ['instance', 'instance_type'], registry=registry)
-        g_node_cpu_use_percent.labels(instance=instance_id, instance_type=instance_type).set(cpu_info.get('cpu_percent'))
+        g_node_cpu_use_percent = Gauge('node_cpu_use_percent', 'The cpu use of node', ['instance_id', 'instance_type'], registry=registry)
+        g_node_cpu_use_percent.labels(instance_id=instance_id, instance_type=instance_type).set(cpu_info.get('cpu_percent'))
         
-        g_node_cpu_use_load5 = Gauge('node_cpu_loadavg_5', 'The cpu load 5 use of node', ['instance', 'instance_type'], registry=registry)
-        g_node_cpu_use_load5.labels(instance=instance_id, instance_type=instance_type).set(cpu_info.get('loadavg_5'))
+        g_node_cpu_use_load5 = Gauge('node_cpu_loadavg_5', 'The cpu load 5 use of node', ['instance_id', 'instance_type'], registry=registry)
+        g_node_cpu_use_load5.labels(instance_id=instance_id, instance_type=instance_type).set(cpu_info.get('loadavg_5'))
         
-        g_node_cpu_use_load10 = Gauge('node_cpu_loadavg_10', 'The cpu load 10 use of node', ['instance', 'instance_type'], registry=registry)
-        g_node_cpu_use_load10.labels(instance=instance_id, instance_type=instance_type).set(cpu_info.get('loadavg_10'))
+        g_node_cpu_use_load10 = Gauge('node_cpu_loadavg_10', 'The cpu load 10 use of node', ['instance_id', 'instance_type'], registry=registry)
+        g_node_cpu_use_load10.labels(instance_id=instance_id, instance_type=instance_type).set(cpu_info.get('loadavg_10'))
         
-        g_node_cpu_use_load15 = Gauge('node_cpu_loadavg_15', 'The cpu load 15 use of node', ['instance', 'instance_type'], registry=registry)
-        g_node_cpu_use_load15.labels(instance=instance_id, instance_type=instance_type).set(cpu_info.get('loadavg_15'))
+        g_node_cpu_use_load15 = Gauge('node_cpu_loadavg_15', 'The cpu load 15 use of node', ['instance_id', 'instance_type'], registry=registry)
+        g_node_cpu_use_load15.labels(instance_id=instance_id, instance_type=instance_type).set(cpu_info.get('loadavg_15'))
         
         push_info('node_cpu_use', registry)
         
